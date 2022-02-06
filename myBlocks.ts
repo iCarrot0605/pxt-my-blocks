e251006bf71976a41c36abe0cb1eb93460befa79
iCarrot0605 <icarrot@mac.com>
2022-02-06 10:07:20 +0900

    add source

---
+++ myBlocks.ts
@@ -0,0 +1,32 @@
+enum TempConvertType {
+    //% block="°C →　°F"
+    CtoF,
+    //% block="°F →　°C"
+    FtoC,
+}
+
+//%color=#2bd92e weight=95 icon="¥uf0ad"
+namespace myBlocks {
+    //% blockId=myBlocks_convertTemperature
+    //% block="convert|%value|%convertType"
+    export function convertTemperature(value: 
+        number, convertType: TempConvertType): number {
+        if (convertType === TempConvertType.CtoF) {
+            return cToF(value);
+        }
+        if (convertType === TempConvertType.FtoC) {
+            return fToC(value);
+        }
+        return Value;
+    }
+
+    //% shim=myBlocks::cToF
+    function ctoF(value: number) {
+        return 32.0; // dummy for sim
+    }
+
+    //% shim=myBlocks::fToC
+    function fToC(value: number) {
+        return -17.78; // dummy for sim
+    }
+}
